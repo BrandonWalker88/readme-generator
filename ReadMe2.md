e449a9559b8165b22514a05cbcf311a6632af254
BrandonWalker88 <Brandon.Walker2008@gmail.com>
2020-09-23 20:56:17 -0400

    added license badge and text finished generatemarkdown to pop readme 2

---
+++ ReadMe2.md
@@ -0,0 +1,23 @@
+# Default Title
+
+  ## Description
+  Default Description
+
+  ## Installation
+  Default Install
+
+  ## Usage
+  Default usage
+
+  ## License
+  This project is licensed under the MIT License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  MIT
+  
+  ## Contributing
+  Default Contributors
+
+  ## Questions
+  Questions regarding this project:
+  GitHub profile: https://github.com/Default username
+  Email: Default Email
--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
   "What is the title of your project?",
   "Please provide a description of your project.",
-  "Table of contents",
   "How do you install your project?",
   "How do you intend for your project to be used?",
   "What licencing was used in your project",
@@ -16,79 +15,83 @@ const questions = [
   "What is your GitHub username",
   "What is your Email",
 ];
-console.log(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {
-  fs.writeToFile(fileName, data, function (err) {
-    if (err) throw Error;
+  fs.writeFile(fileName, data, function (error) {
+    if (error) throw error;
   });
 }
 
 // function to initialize program
 function init() {
-  inquirer.prompt([
-    {
-      type: "input",
-      name: "title",
-      message: questions[0],
-      default: "Default Title",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: questions[1],
-      default: "Default Description",
-    },
-    {
-      type: "input",
-      name: "Install",
-      message: questions[3],
-      default: "Default Install",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: questions[4],
-      default: "Default usage",
-    },
-    {
-      type: "input",
-      name: "licencing",
-      message: questions[5],
-      default: "Default Licencing",
-    },
-    {
-      type: "input",
-      name: "contributors",
-      message: questions[6],
-      default: "Default Contributors",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: questions[7],
-      default: "Default Test",
-    },
-    {
-      type: "input",
-      name: "instructions",
-      message: questions[8],
-      default: "Default Instructions",
-    },
-    {
-      type: "input",
-      name: "username",
-      message: questions[9],
-      default: "Default username",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: questions[10],
-      default: "Default Email",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: questions[0],
+        default: "Default Title",
+      },
+      {
+        type: "input",
+        name: "description",
+        message: questions[1],
+        default: "Default Description",
+      },
+      {
+        type: "input",
+        name: "Installation",
+        message: questions[2],
+        default: "Default Install",
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: questions[3],
+        default: "Default usage",
+      },
+      {
+        type: "list",
+        name: "license",
+        choices: ["MIT", "GNU GPLv3", "no license"],
+        message: questions[4],
+      },
+      {
+        type: "input",
+        name: "contributors",
+        message: questions[5],
+        default: "Default Contributors",
+      },
+      {
+        type: "input",
+        name: "test",
+        message: questions[6],
+        default: "Default Test",
+      },
+      {
+        type: "input",
+        name: "instructions",
+        message: questions[7],
+        default: "Default Instructions",
+      },
+      {
+        type: "input",
+        name: "username",
+        message: questions[8],
+        default: "Default username",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: questions[9],
+        default: "Default Email",
+      },
+    ])
+    .then(function (response) {
+      const markDown = generateMarkdown(response);
+      writeToFile("ReadMe2.md", markDown);
+    });
 }
 
 // function call to initialize program
